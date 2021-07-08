4402805e1f1df759a5098aa48ec24fabe5594fd1
Hugi Thordarson <hugi@karlmenn.is>
2021-07-08 16:11:44 +0000

    Moved utility code to a separate Util class

--- src/main/java/ng/maven/PackageMojo.java
+++ src/main/java/ng/maven/PackageMojo.java
@@ -1,15 +1,8 @@
 package ng.maven;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -46,16 +39,16 @@ public class PackageMojo extends AbstractMojo {
 		// This is the WOA bundle, the destination for our build
 		final WOA woa = WOA.getAtPath( buildPath, applicationName() );
 
-		// This is the name of the JAR file generated for the application
+		// This is the name of the JAR file generated for the application. Lowercase application name with .jar appended.
 		final String appJarFilename = project.getArtifact().getArtifactId().toLowerCase() + ".jar";
 
 		// Copy the main jar to the woa
-		copyFile( artifactPath, woa.javaPath().resolve( appJarFilename ) );
+		Util.copyFile( artifactPath, woa.javaPath().resolve( appJarFilename ) );
 
 		// Start working on that list of paths to add to classpath
 		final List<String> stringsForClasspath = new ArrayList<>();
 
-		// FIXME: I don't like to use strings to represent locations in the tree
+		// FIXME: Not a fan of don't like to using hardcoded strings to represent directory locations
 		stringsForClasspath.add( "Contents/Resources/Java/" + appJarFilename );
 
 		// Copy in the dependency jars
@@ -66,110 +59,25 @@ public class PackageMojo extends AbstractMojo {
 			getLog().debug( "Copying artifact: " + artifact );
 
 			final Path artifactPathInRepository = artifact.getFile().toPath();
-			final Path artifactFolderPath = folder( woa.javaPath().resolve( artifact.getGroupId().replace( ".", "/" ) + "/" + artifact.getArtifactId() + "/" + artifact.getVersion() ) );
+			final Path artifactFolderPath = Util.folder( woa.javaPath().resolve( artifact.getGroupId().replace( ".", "/" ) + "/" + artifact.getArtifactId() + "/" + artifact.getVersion() ) );
 			final Path targetPath = artifactFolderPath.resolve( artifact.getFile().getName() );
 
 			stringsForClasspath.add( targetPath.toString() );
 
-			copyFile( artifactPathInRepository, targetPath );
+			Util.copyFile( artifactPathInRepository, targetPath );
 		}
 
-		copyDirectory( project.getBasedir() + "/src/main/components", woa.resourcesPath().toString() );
-		copyDirectory( project.getBasedir() + "/src/main/resources", woa.resourcesPath().toString() ); // FIXME: This should eventually be woresources
-		copyDirectory( project.getBasedir() + "/src/main/webserver-resources", woa.webServerResourcesPath().toString() );
+		Util.copyDirectory( project.getBasedir() + "/src/main/components", woa.resourcesPath().toString() );
+		Util.copyDirectory( project.getBasedir() + "/src/main/resources", woa.resourcesPath().toString() ); // FIXME: This should eventually be woresources
+		Util.copyDirectory( project.getBasedir() + "/src/main/webserver-resources", woa.webServerResourcesPath().toString() );
 
-		writeToPath( template( "launch-script" ), woa.baseLaunchScriptPath() );
-		makeExecutable( woa.baseLaunchScriptPath() );
+		Util.writeToPath( Util.template( "launch-script" ), woa.baseLaunchScriptPath() );
+		Util.makeExecutable( woa.baseLaunchScriptPath() );
 
-		writeToPath( template( "info-plist" ), woa.contentsPath().resolve( "Info.plist" ) );
-		writeToPath( template( "classpath" ), woa.macosPath().resolve( "MacOSClassPath.txt" ) );
-		writeToPath( template( "classpath" ), woa.unixPath().resolve( "UNIXClassPath.txt" ) );
+		Util.writeToPath( Util.template( "info-plist" ), woa.contentsPath().resolve( "Info.plist" ) );
+		Util.writeToPath( Util.template( "classpath" ), woa.macosPath().resolve( "MacOSClassPath.txt" ) );
+		Util.writeToPath( Util.template( "classpath" ), woa.unixPath().resolve( "UNIXClassPath.txt" ) );
 		// FIXME: Add Windows classpath
-	}
-
-	private static void copyFile( final Path sourcePath, final Path destinationPath ) {
-		try {
-			Files.copy( sourcePath, destinationPath );
-		}
-		catch( final IOException e ) {
-			throw new RuntimeException( e );
-		}
-	}
-
-	/**
-	 * FIXME: Change to accept Paths as parameters
-	 */
-	private static void copyDirectory( String sourceDirectoryLocation, String destinationDirectoryLocation ) {
-		try {
-			Files.walk( Paths.get( sourceDirectoryLocation ) )
-					.forEach( source -> {
-						final Path destination = Paths.get( destinationDirectoryLocation, source.toString().substring( sourceDirectoryLocation.length() ) );
-						try {
-							if( !Files.exists( destination ) ) { // FIXME: This is just a hackyhack
-								Files.copy( source, destination );
-							}
-						}
-						catch( final IOException e ) {
-							throw new RuntimeException( e );
-						}
-					} );
-		}
-		catch( final IOException e ) {
-			throw new RuntimeException( e );
-		}
-	}
-
-	private static void writeToPath( final String string, final Path path ) {
-		try {
-			Files.write( path, string.getBytes( StandardCharsets.UTF_8 ) );
-		}
-		catch( final IOException e ) {
-			throw new RuntimeException( e );
-		}
-	}
-
-	private static void makeExecutable( final Path path ) {
-		Objects.requireNonNull( path );
-
-		try {
-			final Set<PosixFilePermission> perms = new HashSet<>();
-			perms.add( PosixFilePermission.OWNER_READ );
-			perms.add( PosixFilePermission.OWNER_WRITE );
-			perms.add( PosixFilePermission.OWNER_EXECUTE );
-			perms.add( PosixFilePermission.GROUP_READ );
-			perms.add( PosixFilePermission.OTHERS_READ );
-			Files.setPosixFilePermissions( path, perms );
-		}
-		catch( final IOException e ) {
-			e.printStackTrace();
-		}
-	}
-
-	private static String template( final String name ) {
-		try( InputStream stream = PackageMojo.class.getResourceAsStream( "/scripts/" + name + ".template.txt" )) {
-			return new String( b( stream ), StandardCharsets.UTF_8 );
-		}
-		catch( final IOException e ) {
-			throw new RuntimeException( e );
-		}
-	}
-
-	private static byte[] b( InputStream is ) {
-		try {
-			final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-			int nRead;
-			final byte[] data = new byte[16384];
-
-			while( (nRead = is.read( data, 0, data.length )) != -1 ) {
-				buffer.write( data, 0, nRead );
-			}
-
-			return buffer.toByteArray();
-		}
-		catch( final Exception e ) {
-			throw new RuntimeException( e );
-		}
 	}
 
 	/**
@@ -194,7 +102,7 @@ public class PackageMojo extends AbstractMojo {
 		private WOA( final Path woaPath, final String applicationName ) {
 			Objects.requireNonNull( woaPath );
 			Objects.requireNonNull( applicationName );
-			_woaPath = folder( woaPath );
+			_woaPath = Util.folder( woaPath );
 			_applicationName = applicationName;
 		}
 
@@ -203,56 +111,35 @@ public class PackageMojo extends AbstractMojo {
 		}
 
 		public Path contentsPath() {
-			return folder( woaPath().resolve( "Contents" ) );
+			return Util.folder( woaPath().resolve( "Contents" ) );
 		}
 
 		public Path macosPath() {
-			return folder( contentsPath().resolve( "MacOS" ) );
+			return Util.folder( contentsPath().resolve( "MacOS" ) );
 		}
 
 		public Path unixPath() {
-			return folder( contentsPath().resolve( "UNIX" ) );
+			return Util.folder( contentsPath().resolve( "UNIX" ) );
 		}
 
 		public Path windowsPath() {
-			return folder( contentsPath().resolve( "Windows" ) );
+			return Util.folder( contentsPath().resolve( "Windows" ) );
 		}
 
 		public Path resourcesPath() {
-			return folder( contentsPath().resolve( "Resources" ) );
+			return Util.folder( contentsPath().resolve( "Resources" ) );
 		}
 
 		public Path webServerResourcesPath() {
-			return folder( contentsPath().resolve( "WebServerResources" ) );
+			return Util.folder( contentsPath().resolve( "WebServerResources" ) );
 		}
 
 		public Path javaPath() {
-			return folder( resourcesPath().resolve( "Java" ) );
+			return Util.folder( resourcesPath().resolve( "Java" ) );
 		}
 
 		public Path baseLaunchScriptPath() {
 			return woaPath().resolve( _applicationName );
 		}
-	}
-
-	/**
-	 * @return The folder at the given path. Creates the folder if missing, throws an exception if the path exists but is not a folder.
-	 */
-	private static Path folder( final Path path ) {
-		if( Files.exists( path ) ) {
-			if( !Files.isDirectory( path ) ) {
-				throw new IllegalArgumentException( "Given folder path exists but is not a folder" );
-			}
-		}
-		else {
-			try {
-				Files.createDirectories( path );
-			}
-			catch( final IOException e ) {
-				throw new RuntimeException( e );
-			}
-		}
-
-		return path;
 	}
 }
---
+++ src/main/java/ng/maven/Util.java
@@ -0,0 +1,123 @@
+package ng.maven;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Util {
+
+	public static void copyFile( final Path sourcePath, final Path destinationPath ) {
+		try {
+			Files.copy( sourcePath, destinationPath );
+		}
+		catch( final IOException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	/**
+	 * FIXME: Change to accept Paths as parameters
+	 */
+	public static void copyDirectory( String sourceDirectoryLocation, String destinationDirectoryLocation ) {
+		try {
+			Files.walk( Paths.get( sourceDirectoryLocation ) )
+					.forEach( source -> {
+						final Path destination = Paths.get( destinationDirectoryLocation, source.toString().substring( sourceDirectoryLocation.length() ) );
+						try {
+							if( !Files.exists( destination ) ) { // FIXME: This is just a hackyhack
+								Files.copy( source, destination );
+							}
+						}
+						catch( final IOException e ) {
+							throw new RuntimeException( e );
+						}
+					} );
+		}
+		catch( final IOException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	public static void writeToPath( final String string, final Path path ) {
+		try {
+			Files.write( path, string.getBytes( StandardCharsets.UTF_8 ) );
+		}
+		catch( final IOException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	public static void makeExecutable( final Path path ) {
+		Objects.requireNonNull( path );
+	
+		try {
+			final Set<PosixFilePermission> perms = new HashSet<>();
+			perms.add( PosixFilePermission.OWNER_READ );
+			perms.add( PosixFilePermission.OWNER_WRITE );
+			perms.add( PosixFilePermission.OWNER_EXECUTE );
+			perms.add( PosixFilePermission.GROUP_READ );
+			perms.add( PosixFilePermission.OTHERS_READ );
+			Files.setPosixFilePermissions( path, perms );
+		}
+		catch( final IOException e ) {
+			e.printStackTrace();
+		}
+	}
+
+	public static byte[] byteArrayFromInputStream( InputStream is ) {
+		try {
+			final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+	
+			int nRead;
+			final byte[] data = new byte[16384];
+	
+			while( (nRead = is.read( data, 0, data.length )) != -1 ) {
+				buffer.write( data, 0, nRead );
+			}
+	
+			return buffer.toByteArray();
+		}
+		catch( final Exception e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	public static String template( final String name ) {
+		try( InputStream stream = PackageMojo.class.getResourceAsStream( "/scripts/" + name + ".template.txt" )) {
+			return new String( byteArrayFromInputStream( stream ), StandardCharsets.UTF_8 );
+		}
+		catch( final IOException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	/**
+	 * @return The folder at the given path. Creates the folder if missing, throws an exception if the path exists but is not a folder.
+	 */
+	public static Path folder( final Path path ) {
+		if( Files.exists( path ) ) {
+			if( !Files.isDirectory( path ) ) {
+				throw new IllegalArgumentException( "Given folder path exists but is not a folder" );
+			}
+		}
+		else {
+			try {
+				Files.createDirectories( path );
+			}
+			catch( final IOException e ) {
+				throw new RuntimeException( e );
+			}
+		}
+	
+		return path;
+	}
+
+}
