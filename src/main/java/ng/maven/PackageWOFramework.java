33f1c9022b2a7b2753feb11585c8b385171f4263
Hugi Thordarson <hugi@karlmenn.is>
2021-07-10 23:34:26 +0000

    Added comment (Filename extension of install jar is wrong (woframework)=

--- src/main/java/ng/maven/PackageWOFramework.java
+++ src/main/java/ng/maven/PackageWOFramework.java
@@ -9,23 +9,24 @@ import org.apache.maven.project.MavenProject;
 public class PackageWOFramework {
 
 	/**
-	 * FIXME: currently this will only build a maven style project
-	 * FIXME: This duplicates some logic from the WOA build. That's on purpose, to help see the natural common aspects of the WOA build and the framework build, before consolidating common logic.
+	 * FIXME: We need to fix the name of the installed artifact, currenty has the extension "woframework" based on it's packaging // Hugi 2021-07-10
+	 * FIXME: currently this will only build a maven style project // Hugi 2021-07-10
+	 * FIXME: This duplicates some logic from the WOA build. That's on purpose, to help see the natural common aspects of the WOA build and the framework build, before consolidating common logic. // Hugi 2021-07-10
 	 */
 	public void execute( final MavenProject mavenProject, final String woresourcesFolderName ) {
 
 		// The jar file resulting from the compilation of our application project (App.jar)
 		final Path artifactPath = mavenProject.getArtifact().getFile().toPath();
 
-		// FIXME: Flatten components  // Hugi 2021-07-10
+		// FIXME: Flatten components // Hugi 2021-07-10
 		// FIXME: Flatten resources (?)  // Hugi 2021-07-10
-		// FIXME: Both of the above happen in some logic common to both the .WOA and the .framework packaging
+		// FIXME: Both of the above happen in some logic common to both the .WOA and the .framework packaging // Hugi 2021-07-10
 
 		Util.copyContentsOfFolderAtPathToFolderInJar( Paths.get( mavenProject.getBasedir() + "/src/main/components" ), "Resources", artifactPath );
 
 		final Path resourcesSourcePath = Paths.get( mavenProject.getBasedir() + "/src/main/" + woresourcesFolderName );
 
-		// FIXME: Should be totally OK for this not to exist. Check in the WOA build as well, check for WebsServerResources and Components as well
+		// FIXME: Should be totally OK for this not to exist. Check in the WOA build as well, check for WebsServerResources and Components as well // Hugi 2021-07-10
 		if( Files.exists( resourcesSourcePath ) ) {
 			Util.copyContentsOfFolderAtPathToFolderInJar( resourcesSourcePath, "Resources", artifactPath );
 		}
