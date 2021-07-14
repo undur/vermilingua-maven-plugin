57ffa3d3c4cf79f2d92e212ea842f7afbf959ac4
Hugi Thordarson <hugi@karlmenn.is>
2021-07-14 07:34:11 +0000

    Generate Info.plist using our PlistSerialization rather than a template

--- src/main/java/ng/packaging/InfoPlist.java
+++ src/main/java/ng/packaging/InfoPlist.java
@@ -1,20 +1,33 @@
 package ng.packaging;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class InfoPlist {
 
 	public static String make( final String applicationName, final String version, final String jarFileName ) {
-		String infoPlistString = Util.readTemplate( "info-plist" );
-		infoPlistString = infoPlistString.replace( "${NSExecutable}", applicationName );
-		infoPlistString = infoPlistString.replace( "${CFBundleExecutable}", applicationName );
-		infoPlistString = infoPlistString.replace( "${CFBundleIconFile}", "WOAfile.icns" );
-		infoPlistString = infoPlistString.replace( "${CFBundleName}", "WOA" );
-		infoPlistString = infoPlistString.replace( "${CFBundlePackageType}", "APPL" );
-		infoPlistString = infoPlistString.replace( "${CFBundleShortVersionString}", version );
-		infoPlistString = infoPlistString.replace( "${CFBundleVersion}", version );
-		infoPlistString = infoPlistString.replace( "${NSJavaPath}", jarFileName );
-		infoPlistString = infoPlistString.replace( "${NSJavaPathClient}", jarFileName );
+		final var infoPlist = new LinkedHashMap<>();
+		infoPlist.put( "NSExecutable", applicationName );
+		infoPlist.put( "CFBundleDevelopmentRegion", "English" );
+		infoPlist.put( "CFBundleExecutable", applicationName );
+		infoPlist.put( "CFBundleGetInfoString", "" );
+		infoPlist.put( "CFBundleIconFile", "WOAfile.icns" );
+		infoPlist.put( "CFBundleIdentifier", "com.apple.myapp" );
+		infoPlist.put( "CFBundleInfoDictionaryVersion", "6.0" );
+		infoPlist.put( "CFBundleName", "WOA" );
+		infoPlist.put( "CFBundlePackageType", "APPL" );
+		infoPlist.put( "CFBundleShortVersionString", version );
+		infoPlist.put( "CFBundleSignature", "webo" );
+		infoPlist.put( "CFBundleVersion", version );
+		infoPlist.put( "Java", Map.of( "JVMVersion", "1.5+" ) );
+		infoPlist.put( "NSJavaClientRoot", "Contents/WebServerResources/Java" );
+		infoPlist.put( "NSJavaNeeded", "FIXME" ); // FIXME: This is a boolean in the original version, add support for boolean serialization // Hugi 2021-07-14
+		infoPlist.put( "NSJavaPath", Arrays.asList( jarFileName ) );
+		infoPlist.put( "NSJavaPathClient", jarFileName );
+		infoPlist.put( "NSJavaRoot", "Contents/Resources/Java" );
 		// FIXME: Has_WOComponents (for frameworks) // Hugi 2021-07-13
 		// FIXME: NSPrincipalClass (for frameworks) // Hugi 2021-07-13
-		return infoPlistString;
+		return new PlistSerialization( infoPlist ).toString();
 	}
 }
