678ec483556580c3b4915f2163ad90c0fed8e155
Hugi Thordarson <hugi@karlmenn.is>
2021-07-14 08:17:36 +0000

    Experimental framework Info.plist

--- src/main/java/ng/packaging/PackageWOFramework.java
+++ src/main/java/ng/packaging/PackageWOFramework.java
@@ -3,6 +3,8 @@ package ng.packaging;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import ng.packaging.SourceProject.Type;
+
 public class PackageWOFramework {
 
 	/**
@@ -26,5 +28,13 @@ public class PackageWOFramework {
 		if( Files.exists( sourceProject.webServerResourcesPath() ) ) {
 			Util.copyContentsOfFolderAtPathToFolderInJar( sourceProject.webServerResourcesPath(), "WebServerResources", artifactPath );
 		}
+
+		// FIXME: Quick hacking to try out Info.plist generation. Refactor, preferably before anyone sees it // Hugi 2021-07-14
+		final var mp = sourceProject.mavenProject();
+		final var name = mp.getArtifactId();
+		final var version = mp.getVersion();
+		final var mainJarFilename = mp.getArtifact().getArtifactId().toLowerCase() + ".jar";
+		final String infoPlistString = InfoPlist.make( Type.Framework, name, version, mainJarFilename, sourceProject.principalClassName() );
+		Util.writeStringToPathInJar( infoPlistString, "Resources/Info.plist", artifactPath );
 	}
 }
